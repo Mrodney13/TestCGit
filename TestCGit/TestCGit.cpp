4d9b52d6bb80c70a9db72d18fe2ef0ce8abcf32e
Mrodney13 <micahrodney.vfx@gmail.com>
2025-01-21 10:30:47 -0500

    Testing branches  and saving to see if it works

--- TestCGit/TestCGit.cpp
+++ TestCGit/TestCGit.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
-using namespace std; // using this commande make it that you dont have to write "std" in the code std::cout turn in to just "cout"
+using namespace std; 
 
 int main()
 {
-    cout << "Hello Stranger! what is your name?\n"; // This will display result 
-    cout << "My name is Micah" << endl;
-    cout << "Today is Tuesday";
+   
 
     system("pause>0");
 }
