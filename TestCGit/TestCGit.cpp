3c1a412a0ed22c6f26d0a3a5a3ba09b478dd0cdf
Mrodney13 <micahrodney.vfx@gmail.com>
2025-01-21 10:24:45 -0500

    finished tutorial 1

--- TestCGit/TestCGit.cpp
+++ TestCGit/TestCGit.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+using namespace std; // using this commande make it that you dont have to write "std" in the code std::cout turn in to just "cout"
 
 int main()
 {
-    std::cout << "Hello Cpluplus\n"; // This will display result 
+    cout << "Hello Stranger! what is your name?\n"; // This will display result 
+    cout << "My name is Micah" << endl;
+    cout << "Today is Tuesday";
+
     system("pause>0");
 }
