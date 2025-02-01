70eaf82d64daa6e3047de1a82cba4f4caa1eba8e
Mrodney13 <micahrodney.vfx@gmail.com>
2025-01-31 19:46:39 -0500

    Finished Bmi calculator

--- TestCGit/TestCGit.cpp
+++ TestCGit/TestCGit.cpp
@@ -1,8 +1,5 @@
 #include <iostream>
 using namespace std; 
-// instead of typing a new line for each char variable it can be written as line 9
-// each one must be separated by a comma
-// ASCII uses 128 characters (enough for alphabet, numerics and special characters)
 
 int main()
 {
@@ -18,20 +15,27 @@ int main()
 
     bmi = (weight * 703) / (height * height);
 
+
     if (bmi > 29.9)
-    {
+    
         cout << "You are Extremely OverWeight and you BMI is: " << bmi;
-
     else
-
+        
         if (bmi < 13.5)
             cout << "You are Extremely UnderWeight and you BMI is: " << bmi;
         else
-            
-            if (bmi>25 && bmi<=29.9)
 
+            if (bmi > 25 && bmi <= 29.9)
+                cout << "You are OverWeight and you BMI is: " << bmi;
+            else
 
-    }
+                if (bmi < 18.5 && bmi >= 13.5)
+                    cout << "You are UnderWeight and you BMI is: " << bmi;
+                
+                else
+                    cout << "You are NormalWeight and you BMI is: " << bmi;
+   
+   
     
     
    
